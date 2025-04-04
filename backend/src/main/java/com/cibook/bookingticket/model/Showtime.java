8fb76d24ff600b467820cbd8cb72ce0ec230dec1
Kay <109297172+Blueming9511@users.noreply.github.com>
2025-04-04 21:53:59 +0700

    add movie details

--- backend/src/main/java/com/cibook/bookingticket/model/Showtime.java
+++ backend/src/main/java/com/cibook/bookingticket/model/Showtime.java
@@ -1,14 +1,20 @@
 package com.cibook.bookingticket.model;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
+import org.springframework.data.mongodb.core.mapping.FieldType;
 
 import java.sql.Time;
+import java.time.Instant;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.util.Date;
 
 @Data
@@ -20,9 +26,21 @@ public class Showtime {
     @Indexed(unique = true)
     private String showTimeCode;
 
-    private Date date;
-    private Time startTime;
-    private Time endTime;
+//    private Date date;
+//    private Time startTime;
+//    private Time endTime;
+
+    @JsonFormat(pattern = "yyyy-MM-dd")
+    private LocalDate date;
+
+    @JsonFormat(pattern = "HH:mm:ss")
+    private LocalTime startTime;
+
+    @JsonFormat(pattern = "HH:mm:ss")
+    private LocalTime endTime;
+
+
+
     private double price;
     @DBRef
     private Movie movie;
@@ -39,4 +57,6 @@ public class Showtime {
         AVAILABLE,
         FULL,
     }
+
+
 }
