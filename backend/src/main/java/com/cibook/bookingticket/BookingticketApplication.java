f87a3285c04a6a53cf8e9b73a715a719d48b3862
janhae04 <hlchy11997@gmail.com>
2025-04-26 14:43:02 +0700

    -a

--- backend/src/main/java/com/cibook/bookingticket/BookingticketApplication.java
+++ backend/src/main/java/com/cibook/bookingticket/BookingticketApplication.java
@@ -11,9 +11,7 @@ import java.util.Map;
 @SpringBootApplication
 @EnableWebSecurity
 @RestController
-
 public class BookingticketApplication{
-
 	public static void main(String[] args) {
 		SpringApplication.run(BookingticketApplication.class, args);
 	}
